8805c57771419bd3be76361d12c72d44f689bd9d
kowalix007 L.K <kowalix007@gmail.com>
2020-06-16 16:39:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma (message)
+def speak_to_grandma
   if message == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif message == "Hi Nana, how are you?"
